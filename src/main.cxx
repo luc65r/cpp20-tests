232ce25b87b08df16c8afedf7f5c9ad5ae6a0eba
Lucas Ransan <lucas@ransan.tk>
2021-11-27 14:52:57 +0100

    use build2

--- src/main.cxx
+++ src/main.cxx
@@ -1,8 +1,15 @@
 import <iostream>;
+import <ratio>;
+
 import si;
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
-    si::test();
+auto main() -> int {
+    using namespace si;
+    auto a = 4._kg;
+    auto b = .2_kg;
+    auto c = a * b;
+    c *= 54;
+    auto d = c * c;
+    std::cout << d.value << std::endl;
     return 0;
 }
